79617538e8d9813589100869207bfb12a4a4ce3c
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-05-31 10:46:42 +0100

    Add task 2 through 5

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for x in range(0, 100):
+    if (x < 99):
+        print("{:d}{:d}".format((x//10), (x%10)), end=", ")
+    else:
+        print("{:d}{:d}".format((x//10), (x%10)))
+
