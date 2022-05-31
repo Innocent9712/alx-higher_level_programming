c621ed06850538e238bd3765b2689441723ae609
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-05-31 12:05:05 +0100

    Fix pycodestyle error

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for x in range(0, 100):
     if (x < 99):
-        print("{:d}{:d}".format((x//10), (x%10)), end=", ")
+        print("{:d}{:d}".format((x // 10), (x % 10)), end=", ")
     else:
-        print("{:d}{:d}".format((x//10), (x%10)))
-
+        print("{:d}{:d}".format((x // 10), (x % 10)))
