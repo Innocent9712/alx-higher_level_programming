2ecc776896e83f3734ed6595d76d0cff88bd8665
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-06-08 14:26:33 +0100

    Fix bugs

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
+# 12-roman_to_int.py
+
+
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or\
-            roman_string is None:
-        return None
-    roman = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
+    """Converts a roman numeral to an integer."""
+    if (not isinstance(roman_string, str) or
+            roman_string is None):
+        return (0)
+
+    roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
     }
-    roman_keys = list(roman.keys())
-    count = 0
+    num = 0
+
     for i in range(len(roman_string)):
-        if roman_string[i] in roman_keys:
-            if roman_string[i] == roman_keys[0]:
-                if i < len(roman_string) - 1 and \
-                  roman_string[i + 1] != roman_keys[0]:
-                    count -= roman[roman_string[i]]
-                else:
-                    count += roman[roman_string[i]]
-            else:
-                count += roman[roman_string[i]]
-        else:
+        if roman_dict.get(roman_string[i], 0) == 0:
             return (0)
-    return count
+
+        if (i != (len(roman_string) - 1) and
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+                num += roman_dict[roman_string[i]] * -1
+
+        else:
+            num += roman_dict[roman_string[i]]
+    return (num)
