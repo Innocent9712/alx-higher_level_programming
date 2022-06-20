172221bd39503cf14f1785669de5be514d96a11a
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-06-20 16:10:50 +0100

    Add mandatory tasks

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from decimal import DivisionByZero
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    my_list_3 = []
+    for x in range(list_length):
+        value = 0
+        try:
+            value = my_list_1[x] / my_list_2[x]
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            my_list_3.append(value)
+    return my_list_3
