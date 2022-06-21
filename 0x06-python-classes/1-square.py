41cfd0e0b255e06cf0139ea31311b5b4caee3aa4
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-06-21 19:30:18 +0100

    Add requirement

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""Define a class Square"""
+
+
 class Square:
+    """Initialize the Square class"""
     def __init__(self, size):
+        """set size private instance attribute to parameter size"""
         self.__size = size
