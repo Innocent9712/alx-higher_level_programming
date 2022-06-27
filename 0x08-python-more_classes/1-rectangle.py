ac646f45a3271de9c7256bedcefd48baaea6b93b
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-06-27 14:34:26 +0100

    Fix bug

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
         height fields
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
