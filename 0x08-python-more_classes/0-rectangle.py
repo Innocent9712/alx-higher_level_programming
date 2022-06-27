c270d52cf4876f14e3f1c0cae4118ffeb17ed2be
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-06-27 13:10:47 +0100

    Add mandatory tasks

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!usr/bin/python3
+
+"""
+    define a class Rectangle
+"""
+
+
+class Rectangle:
+    """
+        Create an empty rectangle class
+    """
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,51 @@
+#!usr/bin/python3
+
+"""
+    define a class Rectangle
+"""
+
+
+class Rectangle:
+    """
+        Initialze the rectangle class with width and
+        height fields
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+            Get the value of the width field
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+            Set the value of the width field
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+            Get the value of the height field
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+            Set the value of the height field
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
