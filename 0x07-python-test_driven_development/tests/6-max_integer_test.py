0c1463e67d69872582b8db3136127975122877dd
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-06-29 12:08:37 +0100

    Fix bug

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,6 +7,17 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+    """unittest class for mat_integer"""
+    def test_module_docstring(self):
+        """Tests for module docsting"""
+        m = __import__('6-max_integer').__doc__
+        self.assertTrue(len(m) > 1)
+
+    def test_function_docstring(self):
+        """Tests for funstion docstring"""
+        f = max_integer.__doc__
+        self.assertTrue(len(f) > 1)
+
     def test_list_is_itterable(self):
         # with self.assertRaises(TypeError):
         #     max_integer(4)
