89eaffc2751cd82f789b1bd00fa6ff6373641bf0
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-06-29 04:12:15 +0100

    Add advanced tasks

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+import numpy as np
+"""
+Function to multiply two matrixes
+returns a single matrix  of the result
+
+"""
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+        arguments are used as type of int
+    """
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
+    if not (all(isinstance(j, list) for j in m_a)):
+        raise TypeError("m_a must be a list of lists")
+
+    if not (all(isinstance(j, list) for j in m_b)):
+        raise TypeError("m_b must be a list of lists")
+
+    if m_a == [[]] or m_a == []:
+        raise ValueError("m_a can't be empty")
+
+    if m_b == [[]] or m_b == []:
+        raise ValueError("m_b can't be empty")
+
+    if not all(all(isinstance(x, (int, float)) for x in j) for j in m_a):
+        raise TypeError("m_a should contain only integers or floats")
+
+    if not all(all(isinstance(x, (int, float)) for x in j) for j in m_b):
+        raise TypeError("m_b should contain only integers or floats")
+
+    for x in range(len(m_a)):
+        if x < len(m_a) - 1:
+            if not (len(m_a[x]) == len(m_a[x + 1])):
+                raise TypeError("each row of m_a must be of the same size")
+
+    for x in range(len(m_b)):
+        if x < len(m_b) - 1:
+            if not (len(m_b[x]) == len(m_b[x + 1])):
+                raise TypeError("each row of m_b must be of the same size")
+
+    if len(m_a[0]) is not len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+    
+    np_m_a = np.array(m_a)
+    np_m_b = np.array(m_b)
+
+    return (np_m_a @ np_m_b).tolist()
+
+
+# mat_a = [
+#     [1, 2, 3],
+#     [1, 2, 3],
+#     [1, 2, 3],
+#     [1, 2, 3],
+# ]
+
+
+# mat_b = [
+#     [1, 2, 3, 4],
+#     [1, 2, 3, 4],
+#     [1, 2, 3, 4],
+# ]
+# mat_c[0][0] = mat_a[0][0] * mat_b[0][0] + mat_a[0][1] *
+# mat_b[1][0] + mat_a[0][2] * mat_b[2][0]
+#                     1           1               2
+#         1           3               1
+# mat_c[0][1] = mat_a[0][0] * mat_b[0][1] + mat_a[0][1] *
+# mat_b[1][1] + mat_a[0][2] * mat_b[2][1]
+#                     1           2               2
+#          1           3               2
+
+# mat_a = [
+#     [3, 4],
+#     [7, 2],
+#     [5, 9],
+# ]
+
+# mat_b = [
+#     [3, 1, 5],
+#     [6, 9, 7],
+# ]
+
+# print(matrix_mul(mat_a, mat_b))
