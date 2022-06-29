57326940ed4d13414a4a84101f551a4b52ab2cae
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-06-29 12:05:18 +0100

    Fix bug

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -17,3 +17,20 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_list_output(self):
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+
+    def test_one_element(self):
+        """Tests for only one number in the list"""
+        self.assertEqual(max_integer([1]), 1)
+
+    def test_all_negative(self):
+        """Tests for list with all negative numbers"""
+        n = [-6, -50, -75, -1, -100]
+        self.assertEqual(max_integer(n), -1)
+
+    def test_none(self):
+        """Tests for passing none as argument"""
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+if __name__ == "__main__":
+    unittest.main()
