c83358eb962a59209cce2dba2aa383ac1cf1ca5f
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-07-05 23:24:07 +0100

    Fix requirement

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,17 +8,18 @@ import os
 import sys
 
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+if __name__ == "__main__":
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-file_is_present = os.path.exists("add_item.json")
-if not file_is_present:
-    save_to_json_file([], "add_item.json")
+    file_is_present = os.path.exists("add_item.json")
+    if not file_is_present:
+        save_to_json_file([], "add_item.json")
 
-if len(sys.argv) < 2:
-    exit(0)
+    if len(sys.argv) < 2:
+        exit(0)
 
-my_list = load_from_json_file("add_item.json")
-for i in range(1, len(sys.argv)):
-    my_list.append(sys.argv[i])
-save_to_json_file(my_list, "add_item.json")
+    my_list = load_from_json_file("add_item.json")
+    for i in range(1, len(sys.argv)):
+        my_list.append(sys.argv[i])
+    save_to_json_file(my_list, "add_item.json")
