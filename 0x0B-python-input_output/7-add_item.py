dbfa6edfc8093031e774d55d2eab2fd9dece84a8
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-07-05 23:25:37 +0100

    Fix requirement

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,7 +9,8 @@ import sys
 
 
 if __name__ == "__main__":
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = __import__('6-load_from_json_file')\
+        .load_from_json_file
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
     file_is_present = os.path.exists("add_item.json")
