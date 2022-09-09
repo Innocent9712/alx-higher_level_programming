33fa6e3e20a895b28e5f74cddc39d838b95b5d55
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-09-09 09:25:11 +0100

    Add task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,9 +6,10 @@ in the database provided where name starts with 'N'
 import MySQLdb
 from sys import argv
 
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%'")
     states = cur.fetchall()
