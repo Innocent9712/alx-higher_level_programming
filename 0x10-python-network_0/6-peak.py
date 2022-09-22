f1e7aba5738afd634e83866541caff116ef1d973
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-09-22 17:14:13 +0100

    Add task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import math
+
+def find_peak(list_of_integers):
+    if len(list_of_integers) == 0:
+        return None
+    current_index = math.floor(len(list_of_integers)/2)
+    peak = list_of_integers[current_index]
+    prev = current_index - 1
+    next = current_index + 1
+
+    while (prev >= 0 and next <= len(list_of_integers) - 1):
+        if (list_of_integers[prev] > peak):
+            peak = list_of_integers[prev]
+            return peak
+        
+        if (list_of_integers[next] > peak):
+            peak = list_of_integers[next]
+            return peak
+        prev -= 1
+        next -= 1
+    return peak
