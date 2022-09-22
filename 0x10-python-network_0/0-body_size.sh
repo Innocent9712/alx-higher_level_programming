fb0567fd5070467b9741bab1a98af0a4f5766351
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-09-22 15:21:33 +0100

    Fix bug

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-response=$(curl -s -o /dev/null -I -w "%{http_code}" codekami.tech)
-
-if [[ "$response" -eq 200 ]]; then
-    # curl -s -o /dev/null -I -w "%{size_download}" "$1"
-    curl -s -L -I "$1" | awk -v IGNORECASE=1 '/^Content-Length/ { print $2 }'
-fi
+curl -si "$1" | grep Content-Length | tail -c 4
