40610865f076a72d9a552ca1e0da88b1699df76d
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-09-22 16:14:47 +0100

    Add task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL and displays all HTTP methods the server will accept.
 # curl -is -X OPTIONS "$1" | grep -i "allow" | awk '{print $2,$END}'
-curl -is -X OPTIONS "$1" | grep -i "allow" | awk '{$1=""; print}' | cut -c2-
+curl -is -X "OPTIONS" "$1" | grep -i "allow" | awk '{$1=""; print}' | cut -c2-
