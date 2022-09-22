c58a6ac4920ee59f197e2ef27726ae410aaa9c3b
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-09-22 15:33:12 +0100

    Add task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-# response=$(curl -s -o /dev/null -I -w "%{http_code}" "$1")
-
-
-# if [[ "$response" -eq 200 ]]; then
-#     curl -s "$1"
-# fi
-
-curl -sL "$1"
+curl -s "$1"
