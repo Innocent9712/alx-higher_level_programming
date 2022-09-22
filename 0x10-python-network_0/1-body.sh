4b109e227c1ec04cd090cc415b32b190938e1927
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-09-22 16:16:09 +0100

    Add task 3

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Write a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+# Write a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -sL "$1"
