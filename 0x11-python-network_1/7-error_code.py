6a32ed7a137d7301664d4b1020d5aef329170d51
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-09-23 16:24:33 +0100

    Add task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a URL, sends a request to the
+URL and displays the body of the response (decoded in utf-8).
+You must use requests and sys
+"""
+import requests
+from sys import argv
+
+res = requests.get(argv[1])
+try:
+    res.raise_for_status()
+    print(res.text)
+except requests.HTTPError:
+    print(f"Error code: {res.status_code}")
