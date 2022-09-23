91deb9000740a7be8f8816e57186b6b88b9a6cd8
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-09-23 14:19:14 +0100

    Fix output bug

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ import urllib.request
 url = "https://alx-intranet.hbtn.io/status"
 with urllib.request.urlopen(url) as response:
     res = response.read()
-    print("Bodyresponse:")
+    print("Body response:")
     print(f'\t- type: {type(res)}')
     print(f'\t- content: {res}')
     print(f"\t- utf8 content: {res.decode('utf8')}")
