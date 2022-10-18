9b02fab6ea89f2343d8221f9433d7d89e2286d79
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-10-18 13:13:42 +0100

    Fix bug

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-// Write a script that reads and prints the content of a file.
-const fs = require('node:fs');
-const args = require('node:process').argv;
+const fs = require('fs');
 
-fs.readFile(args[2], 'utf-8', function (err, data) {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    return console.error(err);
+    console.log(err);
+  } else {
+    console.log(data.toString());
   }
-  console.log(data.toString());
 });
