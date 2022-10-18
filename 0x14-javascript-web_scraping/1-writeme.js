0064346d037566a236357e93770bd7038febfbc2
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-10-18 13:19:21 +0100

    Fix bug

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Write a script that writes a string to a file.
-const fs = require('node:fs');
+const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err, data) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
   if (err) {
     console.log(err);
   }
