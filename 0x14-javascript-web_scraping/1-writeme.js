8c034842c56c4114abb20b6fc22bfec4906a9fea
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-10-18 11:33:54 +0100

    Add task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Write a script that writes a string to a file.
+const fs = require('node:fs');
+const args = require('node:process').argv;
+
+fs.writeFile(args[2], args[3], 'utf-8', function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+});
